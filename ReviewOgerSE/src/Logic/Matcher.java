0eb959800fa2ea5959878ad56b86084f9f754b3f
verenak <verena@verena-Laptop>
2014-04-12 17:19:20 +0200

    match function header

--- ReviewOgerSE/src/Logic/Matcher.java
+++ ReviewOgerSE/src/Logic/Matcher.java
@@ -1,5 +1,18 @@
+/**
+ * This class provides the main logical functions to match participants to reviews
+ */
 package Logic;
 
-public class Matcher {
+import data.ReviewPlan;
 
+public class Matcher {
+	/**
+	 * This function matches all participants to the reviews
+	 * 
+	 * @return A review plan with a solution
+	 */
+	public ReviewPlan MatchReview() {
+		// TODO
+		return null;
+	}
 }
