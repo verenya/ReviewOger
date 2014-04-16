d6f09db6860e9f06ab254428582d90ba9409cd49
verenak <verena@verena-Laptop>
2014-04-16 15:01:57 +0200

    merge

---
+++ ReviewOgerSE/src/logic/Matcher.java
@@ -0,0 +1,18 @@
+/**
+ * This class provides the main logical functions to match participants to reviews
+ */
+package logic;
+
+import data.ReviewPlan;
+
+public class Matcher {
+	/**
+	 * This function matches all participants to the reviews
+	 * 
+	 * @return A review plan with a solution
+	 */
+	public ReviewPlan MatchReview() {
+		// TODO
+		return null;
+	}
+}
---
+++ ReviewOgerSE/src/logic/Pool.java
@@ -0,0 +1,73 @@
+/**
+ * This class represents one pool for a review. A pool holds all participants which can be added to a review. These participants have no other review in the same time slot, have not the same group as the author of the review and have not participated in the maximum amount of reviews for each participabt.  
+ */
+package logic;
+
+import java.util.ArrayList;
+
+import data.Participant;
+import data.Review;
+import data.ReviewPlan;
+
+public class Pool {
+
+	/**
+	 * The possible participants for a review
+	 */
+	private ArrayList<Participant> participantList = new ArrayList<Participant>();
+
+	/**
+	 * Generates a pool of possible participants for the given review
+	 * 
+	 * @param review
+	 *            The review the pool is generated for
+	 * @return True if enough participants for all free positions are found,
+	 *         else false
+	 */
+	protected boolean generatePoolForReview(Review review) {
+		for (Participant p : ReviewPlan.participants) {
+			// must not be the same group as the author and not too many reviews
+			if (!(p.getGroupNumber() == review.getAuthor().getGroupNumber())
+					&& (p.getNumberOfReviews() < ReviewPlan.maxNumberOfReviews)) {
+				participantList.add(p);
+			}
+		}
+
+		// number of reviewers + moderator
+		if (review.getAuthorIsScribe()) {
+			if (participantList.size() >= review.getNumberOfReviewers() + 1) {
+				return true;
+			}
+			// //number of reviewers + moderator + scribe
+		} else {
+			if (participantList.size() >= review.getNumberOfReviewers() + 2) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Removes the given participant from the pool
+	 * 
+	 * @param participantToRemove
+	 *            The participant which should be removed from the pool
+	 */
+	protected void removeParticipant(Participant participantToRemove) {
+		participantList.remove(participantToRemove);
+	}
+
+	/**
+	 * @return the current size of the pool
+	 */
+	protected int getCurrentPoolsize() {
+		return participantList.size();
+	}
+
+	/**
+	 * @return The participants of the pool
+	 */
+	protected ArrayList<Participant> getPool() {
+		return participantList;
+	}
+}
