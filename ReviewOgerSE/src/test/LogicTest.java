3f757a30ff7c0b64ff01398e0e8e77a4892232a6
Jan <Jan-Peter.Ostberg@informatik.uni-stuttgart.de>
2014-04-22 14:11:15 +0200

    Repair commit

---
+++ ReviewOgerSE/src/test/LogicTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogicTest {
+	
+	@Test
+	public void testSomething(){
+		fail("No test yet implemented!");
+	}
+
+
+}
