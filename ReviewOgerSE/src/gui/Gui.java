51bd5aa23ad90802668628ee2e9735f6927bc0be
verenak <verena.kaefer@googlemail.com>
2014-04-30 14:05:15 +0200

    gui

---
+++ ReviewOgerSE/src/gui/Gui.java
@@ -0,0 +1,321 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JList;
+import javax.swing.ListModel;
+
+import javax.swing.JTextField;
+
+import data.Participant;
+import data.ParticipantListModel;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Insets;
+import javax.swing.JMenuItem;
+
+public class Gui extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField reviewerField;
+	private JTextField reviewerNumberTextField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui frame = new Gui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Gui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 967, 414);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem menuSave = new JMenuItem("Speichern");
+		mnFile.add(menuSave);
+		
+		JMenuItem menuLoadParticipant = new JMenuItem("Teilnehmer einlesen");
+		mnFile.add(menuLoadParticipant);
+		
+		JMenuItem menuLoadRoom = new JMenuItem("Räume einlesen");
+		mnFile.add(menuLoadRoom);
+		
+		JMenuItem menuExit = new JMenuItem("Schließen");
+		mnFile.add(menuExit);
+		
+		JMenu menuEdit = new JMenu("Edit");
+		menuBar.add(menuEdit);
+		
+		JMenu menuParticipant = new JMenu("Teilnehmer");
+		menuEdit.add(menuParticipant);
+		
+		JMenuItem menuAddParticipant = new JMenuItem("hinzufügen");
+		menuParticipant.add(menuAddParticipant);
+		
+		JMenuItem menuDeleteParticipant = new JMenuItem("löschen");
+		menuParticipant.add(menuDeleteParticipant);
+		
+		JMenuItem menuEditParticipant = new JMenuItem("bearbeiten");
+		menuParticipant.add(menuEditParticipant);
+		
+		JMenu menuRoom = new JMenu("Raum");
+		menuEdit.add(menuRoom);
+		
+		JMenuItem menuAddRoom = new JMenuItem("hinzufügen");
+		menuRoom.add(menuAddRoom);
+		
+		JMenuItem menuDeleteRoom = new JMenuItem("löschen");
+		menuRoom.add(menuDeleteRoom);
+		
+		JMenuItem menuEditRoom = new JMenuItem("bearbeiten");
+		menuRoom.add(menuEditRoom);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{276, 296, 314};
+		gridBagLayout.rowHeights = new int[]{43, 56, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		ListModel<Participant> participantModel = new ParticipantListModel();
+		
+		JPanel participantPanel = new JPanel();
+		GridBagConstraints gbc_participantPanel = new GridBagConstraints();
+		gbc_participantPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_participantPanel.gridx = 0;
+		gbc_participantPanel.gridy = 0;
+		getContentPane().add(participantPanel, gbc_participantPanel);
+		GridBagLayout gbl_participantPanel = new GridBagLayout();
+		gbl_participantPanel.columnWidths = new int[]{193, 0};
+		gbl_participantPanel.rowHeights = new int[]{211, 65, 0};
+		gbl_participantPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_participantPanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		participantPanel.setLayout(gbl_participantPanel);
+		
+		JList<Participant> participantList = new JList<Participant>();
+		participantList.setModel(participantModel);
+		GridBagConstraints gbc_participantList = new GridBagConstraints();
+		gbc_participantList.fill = GridBagConstraints.BOTH;
+		gbc_participantList.insets = new Insets(0, 0, 5, 0);
+		gbc_participantList.gridx = 0;
+		gbc_participantList.gridy = 0;
+		participantPanel.add(participantList, gbc_participantList);
+		
+		JPanel participantButtonPanle = new JPanel();
+		GridBagConstraints gbc_participantButtonPanle = new GridBagConstraints();
+		gbc_participantButtonPanle.fill = GridBagConstraints.BOTH;
+		gbc_participantButtonPanle.gridx = 0;
+		gbc_participantButtonPanle.gridy = 1;
+		participantPanel.add(participantButtonPanle, gbc_participantButtonPanle);
+		GridBagLayout gbl_participantButtonPanle = new GridBagLayout();
+		gbl_participantButtonPanle.columnWidths = new int[]{115, 136, 0};
+		gbl_participantButtonPanle.rowHeights = new int[]{25, 25, 0};
+		gbl_participantButtonPanle.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_participantButtonPanle.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		participantButtonPanle.setLayout(gbl_participantButtonPanle);
+		
+		JButton participantAddButton = new JButton("Hinzufügen");
+		GridBagConstraints gbc_participantAddButton = new GridBagConstraints();
+		gbc_participantAddButton.anchor = GridBagConstraints.NORTH;
+		gbc_participantAddButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_participantAddButton.insets = new Insets(0, 0, 5, 5);
+		gbc_participantAddButton.gridx = 0;
+		gbc_participantAddButton.gridy = 0;
+		participantButtonPanle.add(participantAddButton, gbc_participantAddButton);
+		
+		JButton participantDeleteButton = new JButton("Löschen");
+		GridBagConstraints gbc_participantDeleteButton = new GridBagConstraints();
+		gbc_participantDeleteButton.anchor = GridBagConstraints.NORTH;
+		gbc_participantDeleteButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_participantDeleteButton.insets = new Insets(0, 0, 5, 0);
+		gbc_participantDeleteButton.gridx = 1;
+		gbc_participantDeleteButton.gridy = 0;
+		participantButtonPanle.add(participantDeleteButton, gbc_participantDeleteButton);
+		
+		JButton participantEditButton = new JButton("Bearbeiten");
+		GridBagConstraints gbc_participantEditButton = new GridBagConstraints();
+		gbc_participantEditButton.fill = GridBagConstraints.BOTH;
+		gbc_participantEditButton.gridwidth = 2;
+		gbc_participantEditButton.gridx = 0;
+		gbc_participantEditButton.gridy = 1;
+		participantButtonPanle.add(participantEditButton, gbc_participantEditButton);
+		
+		JPanel roomPanel = new JPanel();
+		GridBagConstraints gbc_roomPanel = new GridBagConstraints();
+		gbc_roomPanel.fill = GridBagConstraints.VERTICAL;
+		gbc_roomPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_roomPanel.gridx = 1;
+		gbc_roomPanel.gridy = 0;
+		getContentPane().add(roomPanel, gbc_roomPanel);
+		GridBagLayout gbl_roomPanel = new GridBagLayout();
+		gbl_roomPanel.columnWidths = new int[]{317, 0};
+		gbl_roomPanel.rowHeights = new int[]{0, 0};
+		gbl_roomPanel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_roomPanel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		roomPanel.setLayout(gbl_roomPanel);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		roomPanel.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{193, 0};
+		gbl_panel.rowHeights = new int[]{211, 65, 0};
+		gbl_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JList<Participant> roomList = new JList<Participant>();
+		GridBagConstraints gbc_roomList = new GridBagConstraints();
+		gbc_roomList.fill = GridBagConstraints.BOTH;
+		gbc_roomList.insets = new Insets(0, 0, 5, 0);
+		gbc_roomList.gridx = 0;
+		gbc_roomList.gridy = 0;
+		panel.add(roomList, gbc_roomList);
+		
+		JPanel roomButtonPanel = new JPanel();
+		GridBagConstraints gbc_roomButtonPanel = new GridBagConstraints();
+		gbc_roomButtonPanel.fill = GridBagConstraints.BOTH;
+		gbc_roomButtonPanel.gridx = 0;
+		gbc_roomButtonPanel.gridy = 1;
+		panel.add(roomButtonPanel, gbc_roomButtonPanel);
+		GridBagLayout gbl_roomButtonPanel = new GridBagLayout();
+		gbl_roomButtonPanel.columnWidths = new int[]{115, 136, 0};
+		gbl_roomButtonPanel.rowHeights = new int[]{25, 25, 0};
+		gbl_roomButtonPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_roomButtonPanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		roomButtonPanel.setLayout(gbl_roomButtonPanel);
+		
+		JButton buttonroomAddButton = new JButton("Hinzufügen");
+		GridBagConstraints gbc_buttonroomAddButton = new GridBagConstraints();
+		gbc_buttonroomAddButton.fill = GridBagConstraints.BOTH;
+		gbc_buttonroomAddButton.insets = new Insets(0, 0, 5, 5);
+		gbc_buttonroomAddButton.gridx = 0;
+		gbc_buttonroomAddButton.gridy = 0;
+		roomButtonPanel.add(buttonroomAddButton, gbc_buttonroomAddButton);
+		
+		JButton roomDeleteButton = new JButton("Löschen");
+		GridBagConstraints gbc_roomDeleteButton = new GridBagConstraints();
+		gbc_roomDeleteButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_roomDeleteButton.insets = new Insets(0, 0, 5, 0);
+		gbc_roomDeleteButton.gridx = 1;
+		gbc_roomDeleteButton.gridy = 0;
+		roomButtonPanel.add(roomDeleteButton, gbc_roomDeleteButton);
+		
+		JButton roomEditButton = new JButton("Bearbeiten");
+		GridBagConstraints gbc_roomEditButton = new GridBagConstraints();
+		gbc_roomEditButton.fill = GridBagConstraints.BOTH;
+		gbc_roomEditButton.gridwidth = 2;
+		gbc_roomEditButton.gridx = 0;
+		gbc_roomEditButton.gridy = 1;
+		roomButtonPanel.add(roomEditButton, gbc_roomEditButton);
+		
+		JPanel optionPanel = new JPanel();
+		GridBagConstraints gbc_optionPanel = new GridBagConstraints();
+		gbc_optionPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_optionPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_optionPanel.anchor = GridBagConstraints.NORTH;
+		gbc_optionPanel.gridx = 2;
+		gbc_optionPanel.gridy = 0;
+		getContentPane().add(optionPanel, gbc_optionPanel);
+		GridBagLayout gbl_optionPanel = new GridBagLayout();
+		gbl_optionPanel.columnWidths = new int[]{123, 39, -18, 0};
+		gbl_optionPanel.rowHeights = new int[]{43, 0, 0, 0, 32, 0, 0};
+		gbl_optionPanel.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_optionPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		optionPanel.setLayout(gbl_optionPanel);
+		
+		JLabel reviewerLabel = new JLabel("  Anzahl Gutachter: ");
+		GridBagConstraints gbc_reviewerLabel = new GridBagConstraints();
+		gbc_reviewerLabel.fill = GridBagConstraints.BOTH;
+		gbc_reviewerLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_reviewerLabel.gridx = 0;
+		gbc_reviewerLabel.gridy = 1;
+		optionPanel.add(reviewerLabel, gbc_reviewerLabel);
+		
+		reviewerNumberTextField = new JTextField();
+		reviewerNumberTextField.setColumns(2);
+		GridBagConstraints gbc_reviewerNumberTextField = new GridBagConstraints();
+		gbc_reviewerNumberTextField.insets = new Insets(0, 0, 5, 5);
+		gbc_reviewerNumberTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_reviewerNumberTextField.gridx = 1;
+		gbc_reviewerNumberTextField.gridy = 1;
+		optionPanel.add(reviewerNumberTextField, gbc_reviewerNumberTextField);
+		
+		JCheckBox scribeIsAuthorCheckbox = new JCheckBox("Notar ist Author");
+		GridBagConstraints gbc_scribeIsAuthorCheckbox = new GridBagConstraints();
+		gbc_scribeIsAuthorCheckbox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_scribeIsAuthorCheckbox.insets = new Insets(0, 0, 5, 5);
+		gbc_scribeIsAuthorCheckbox.gridwidth = 2;
+		gbc_scribeIsAuthorCheckbox.gridx = 0;
+		gbc_scribeIsAuthorCheckbox.gridy = 2;
+		optionPanel.add(scribeIsAuthorCheckbox, gbc_scribeIsAuthorCheckbox);
+		
+		JCheckBox moderatorNotReviewerGroupCheckbox = new JCheckBox("Moderator nicht aus Gutachter-Gruppe");
+		GridBagConstraints gbc_moderatorNotReviewerGroupCheckbox = new GridBagConstraints();
+		gbc_moderatorNotReviewerGroupCheckbox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_moderatorNotReviewerGroupCheckbox.insets = new Insets(0, 0, 5, 5);
+		gbc_moderatorNotReviewerGroupCheckbox.gridwidth = 2;
+		gbc_moderatorNotReviewerGroupCheckbox.gridx = 0;
+		gbc_moderatorNotReviewerGroupCheckbox.gridy = 3;
+		optionPanel.add(moderatorNotReviewerGroupCheckbox, gbc_moderatorNotReviewerGroupCheckbox);
+		
+		JLabel exitOptionLabel = new JLabel("  Abbruchbedingung:");
+		GridBagConstraints gbc_exitOptionLabel = new GridBagConstraints();
+		gbc_exitOptionLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_exitOptionLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_exitOptionLabel.gridx = 0;
+		gbc_exitOptionLabel.gridy = 4;
+		optionPanel.add(exitOptionLabel, gbc_exitOptionLabel);
+		
+		JComboBox exitOptionComboBox = new JComboBox();
+		exitOptionComboBox.setModel(new DefaultComboBoxModel(new String[] {"manueller Abbruch", "feste Zeit", "feste Anzahl"}));
+		GridBagConstraints gbc_exitOptionComboBox = new GridBagConstraints();
+		gbc_exitOptionComboBox.insets = new Insets(0, 0, 0, 5);
+		gbc_exitOptionComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_exitOptionComboBox.gridwidth = 2;
+		gbc_exitOptionComboBox.gridx = 0;
+		gbc_exitOptionComboBox.gridy = 5;
+		optionPanel.add(exitOptionComboBox, gbc_exitOptionComboBox);
+		
+		JButton startButton = new JButton("Berechnung beginnen");
+		GridBagConstraints gbc_startButton = new GridBagConstraints();
+		gbc_startButton.insets = new Insets(0, 0, 5, 0);
+		gbc_startButton.fill = GridBagConstraints.BOTH;
+		gbc_startButton.gridwidth = 3;
+		gbc_startButton.gridx = 0;
+		gbc_startButton.gridy = 1;
+		getContentPane().add(startButton, gbc_startButton);
+		
+		
+	}
+
+}
